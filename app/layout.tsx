85d605fdbe2d1495fd8b39244899d2238856ec22
Raúl Montejo <raul@clevelandcoder.com>
2025-04-05 13:18:05 -0400

    feat: leverage Metadata API
    Transfers HTML document metadata to NextJS Metadata object for
    simplicity and maintainability.

--- app/layout.tsx
+++ app/layout.tsx
@@ -15,6 +15,40 @@ export const metadata: Metadata = {
   title: 'Tiny Erica Jewelry',
   description:
     'Tiny Erica Jewelry features hand-made jewelry and custom jewelry design that integrates elements of nature into a fresh, clean aesthetic',
+  keywords: 'tiny erica, jewelry, jewelry design, custom, hand-made, silver, rings, charms, gems',
+  authors: [{ name: 'Raúl Montejo' }],
+  creator: 'Tiny Erica Jewelry',
+  publisher: 'Tiny Erica Jewelry',
+  alternates: {
+    canonical: 'https://tinyerica.com',
+  },
+  icons: {
+    icon: [
+      { url: '/favicon-32x32.png', sizes: '32x32', type: 'image/png' },
+      { url: '/favicon-16x16.png', sizes: '16x16', type: 'image/png' },
+    ],
+    apple: [{ url: '/apple-touch-icon.png', sizes: '180x180', type: 'image/png' }],
+    other: [
+      {
+        rel: 'mask-icon',
+        url: '/safari-pinned-tab.svg',
+        color: '#9a7f62',
+      },
+    ],
+  },
+  manifest: '/site.webmanifest',
+  themeColor: '#ffffff',
+  viewport: 'width=device-width, initial-scale=1',
+  robots: 'index, follow',
+  openGraph: {
+    title: 'Tiny Erica Jewelry',
+    description:
+      'Tiny Erica Jewelry features hand-made jewelry and custom jewelry design that integrates elements of nature into a fresh, clean aesthetic',
+    url: 'http://tinyerica.com',
+    siteName: 'Tiny Erica Jewelry',
+    locale: 'en_US',
+    type: 'website',
+  },
 };
 
 export default function RootLayout({
@@ -24,23 +58,6 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en-US">
-      <head>
-        <meta
-          name="keywords"
-          content="tiny erica, jewelry, jewelry design, custom, hand-made, silver, rings, charms, gems"
-        />
-        <meta name="author" content="Raúl Montejo" />
-        <meta name="copyright" content="Tiny Erica Jewelry" />
-        <link rel="canonical" href="http://tinyerica.com" />
-        <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
-        <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
-        <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
-        <link rel="manifest" href="/site.webmanifest" />
-        <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#9a7f62" />
-        <meta name="msapplication-TileColor" content="#da532c" />
-        <meta name="theme-color" content="#ffffff" />
-      </head>
-
       <body className={lato.variable}>
         <header id="header">
           <Logo />
