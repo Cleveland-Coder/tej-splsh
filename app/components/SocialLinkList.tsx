dcea1886c946e0126661cf88109c16482b234c2e
Raúl Montejo <raul@clevelandcoder.com>
2025-04-05 19:30:18 -0400

    refactor(SocialLinkList)
    Improves component structure and maintainability
    
    - Extract SocialLink into reusable component
    - Use configuration array for social links
    - Use title as key instead of index
    - Alphabetize object properties
    - Add TypeScript interface for type safety

--- app/components/SocialLinkList.tsx
+++ app/components/SocialLinkList.tsx
@@ -1,26 +1,48 @@
+interface SocialLinkProps {
+  href: string;
+  iconClass: string;
+  title: string;
+}
+
+function SocialLink({ href, title, iconClass }: SocialLinkProps) {
+  return (
+    <li>
+      <a href={href} title={title}>
+        <span className={`icon-social fa ${iconClass}`}></span>
+      </a>
+    </li>
+  );
+}
+
+const socialLinks: SocialLinkProps[] = [
+  {
+    href: '//instagram.com/tinyerica',
+    iconClass: 'fa-instagram',
+    title: 'instagram',
+  },
+  {
+    href: '//facebook.com/tinyerica',
+    iconClass: 'fa-facebook-square',
+    title: 'facebook',
+  },
+  {
+    href: '//pinterest.com/tinyerica',
+    iconClass: 'fa-pinterest-square',
+    title: 'pinterest',
+  },
+  {
+    href: '//twitter.com/tinyerica',
+    iconClass: 'fa-twitter-square',
+    title: 'twitter',
+  },
+];
+
 function SocialLinkList() {
   return (
     <ul className="list-social">
-      <li>
-        <a href="//instagram.com/tinyerica" title="instagram">
-          <span className="icon-social fa fa-instagram"></span>
-        </a>
-      </li>
-      <li>
-        <a href="//facebook.com/tinyerica" title="facebook">
-          <span className="icon-social fa fa-facebook-square"></span>
-        </a>
-      </li>
-      <li>
-        <a href="//pinterest.com/tinyerica" title="pinterest">
-          <span className="icon-social fa fa-pinterest-square"></span>
-        </a>
-      </li>
-      <li>
-        <a href="//twitter.com/tinyerica" title="twitter">
-          <span className="icon-social fa fa-twitter-square"></span>
-        </a>
-      </li>
+      {socialLinks.map((link) => (
+        <SocialLink key={link.title} {...link} />
+      ))}
     </ul>
   );
 }
