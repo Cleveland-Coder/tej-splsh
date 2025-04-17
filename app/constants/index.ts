029d288085cc11dde94bd16c9d294f20cd85df88
Raúl Montejo <raul@clevelandcoder.com>
2025-04-17 09:46:42 -0400

    clean: remove unused font awesome script url

--- app/constants/index.ts
+++ app/constants/index.ts
@@ -7,5 +7,3 @@ export const EXTERNAL_LINK_PROPS: ExternalLinkProps = {
   rel: '_blank',
   target: 'noopener noreferrer',
 } as const;
-
-export const FONT_AWESOME_SCRIPT_URL = 'https://use.fontawesome.com/f6e0e51e9b.js' as const;
