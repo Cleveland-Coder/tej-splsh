8750546a2e34a9c2725b863b452096e16705b78e
Raúl Montejo <raul@clevelandcoder.com>
2025-05-23 01:06:53 -0400

    add design colors to theme constants

--- app/constants/theme.ts
+++ app/constants/theme.ts
@@ -1,11 +1,16 @@
 export const THEME = {
   colors: {
     background: '#ffffff',
+    blindForest: '#233000',
+    butterHoney: '#f5e5a9',
+    fullMoon: '#f4f3e1',
+    goGoLime: '#c5be6c',
     primary: '#9a7f62',
     social: {
       facebook: '#3b5998',
       instagram: '#3f729b',
       pinterest: '#cc2127',
     },
+    warmWassail: '#a57068',
   },
 } as const;
