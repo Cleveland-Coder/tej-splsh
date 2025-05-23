0f8fbea1bc5961df25c811e175d546bbb2ffb6d8
Raúl Montejo <raul@clevelandcoder.com>
2025-05-23 00:51:12 -0400

    Add desired google fonts to config

--- app/config/fonts.ts
+++ app/config/fonts.ts
@@ -1,10 +1,29 @@
-import { Lato } from 'next/font/google';
+import { Arsenal, Lato, Open_Sans } from 'next/font/google';
+
+export const arsenalBoldItalic = Arsenal({
+  weight: '700',
+  style: 'italic',
+  subsets: ['latin'],
+  display: 'swap',
+  variable: '--font-arsenal-bold-italic',
+  fallback: ['Helvetica', 'Verdana', 'sans-serif'],
+  preload: false,
+});
 
 export const lato = Lato({
   weight: '300',
   subsets: ['latin'],
   display: 'swap',
   variable: '--font-lato',
+  fallback: ['Helvetica', 'Verdana', 'sans-serif'],
+  preload: false,
+});
+
+export const openSans = Open_Sans({
+  weight: '400',
+  subsets: ['latin'],
+  display: 'swap',
+  variable: '--font-open-sans',
   fallback: ['Helvetica', 'Verdana', 'sans-serif'],
   preload: false,
 });
