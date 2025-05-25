76f3bac3dd21988eb37f58a51e43bc3f3e5fa2c1
Raúl Montejo <raul@clevelandcoder.com>
2025-05-25 00:25:36 -0400

    feat: add intro text to new homepage

--- app/b/page.tsx
+++ app/b/page.tsx
@@ -6,6 +6,9 @@ import Logo from '~components/Logo';
 // Internal constants
 import { THEME } from '~constants/theme';
 
+// Internal utilities and config
+import { arsenalBoldItalic } from '~config/fonts';
+
 // Internal styles
 import { headerClassnames, layoutClassnames } from './tailwind';
 
@@ -16,7 +19,16 @@ function Home() {
         <Logo fill={THEME.colors.blindForest} />
       </header>
       <main>
-        <Container>
+        <Container className="max-w-[600px]">
+          <div className={`${arsenalBoldItalic.className} text-center`}>
+            <h1 className="text-4xl font-bold">Tiny Erica Jewelry</h1>
+            <p className="text-lg mt-4">
+              Celebrate who you are and who you love with a bespoke collection of 14k gold and
+              sterling silver jewelry designed on paper, crafted by hand, and made to last&hellip;
+            </p>
+          </div>
+        </Container>
+        <Container className="max-w-[360px] mt-12 ">
           <ActionLinkList />
         </Container>
       </main>
