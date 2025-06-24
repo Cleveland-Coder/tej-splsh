922c9a76c3b76ab81519584b647730b91fb13f7b
Raúl Montejo <raul@clevelandcoder.com>
2025-06-24 18:29:41 -0400

    feat: use new homepage
    Update app router files so website uses new homepage design

--- app/tailwind.ts
+++ app/tailwind.ts
@@ -2,14 +2,10 @@
  * Tailwind utility classes for the home page
  */
 export const headerClassnames = `
-  mb-4
-`;
+  my-8
+`.trim();
 
 export const layoutClassnames = `
-  min-h-screen
-  flex
-  flex-col
-  justify-center
   text-center
   antialiased
 `.trim();
